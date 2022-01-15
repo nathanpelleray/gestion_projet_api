70a41136044e0d7b53aa9e6766515bc373484655
nathan pelleray <pelleray.nathan@gmail.com>
2022-01-15 11:55:30 +0100

    process request

--- api.py
+++ api.py
@@ -2,12 +2,14 @@ from fastapi import FastAPI
 import threading
 import time
 import socket
+from request import *
 
 
 """
 VARIABLES EN COMMUN
 """
-client = None
+info = {}
+raspberry = None
 
 """
 GESTION DES RASPBERRY
@@ -20,6 +22,54 @@ FONCTION UTILE
 def envoiMessage(client, message):
     client.send(message.encode("utf-8"))
 
+
+def processRequest(request):
+    data = request.dict()
+    
+    # Switch 1 (RED)
+    if data["switch1"] == "on":
+        envoiMessage(raspberry, "1:1")
+    else:
+        print(f"switch1 : {data['switch1']}")
+        envoiMessage(raspberry, "1:0")
+
+    time.sleep(0.08)
+
+    # Switch 2 (GREEN)
+    if data["switch2"] == "on":
+        print(f"switch2 : {data['switch2']}")
+        envoiMessage(raspberry, "2:1")
+    else:
+        envoiMessage(raspberry, "2:0")
+
+    time.sleep(0.08)
+
+    # Switch 3 (BLUE)
+    if data["switch3"] == "on":
+        envoiMessage(raspberry, "3:1")
+    else:
+        print(f"switch3 : {data['switch3']}")
+        envoiMessage(raspberry, "3:0")
+
+    time.sleep(0.08)
+
+    # Range 1
+    envoiMessage(raspberry, f"04:{data['range1']}")
+    print(f"04:{data['range1']}")
+    time.sleep(1)
+
+    # Range 2
+    envoiMessage(raspberry, f"05:{data['range2']}")
+    print(f"05:{data['range2']}")
+    time.sleep(0.1)
+
+    # Range 3
+    envoiMessage(raspberry, f"06:{data['range3']}")
+    print(f"06:{data['range3']}")
+    time.sleep(0.1)
+
+    # Range 4
+    envoiMessage(raspberry, f"10:{data['range4']}")
 
 
 """
@@ -38,7 +88,16 @@ def serveur():
     while True:
         message = client.recv(255)
         message = message.decode("utf-8")
-        print(message)
+
+        message_content = message.split(":")
+
+        # info[type(message_content[0])] = message_content[1]
+        # if message_content[0] == "09":
+        #     info["09"] = message_content[1]
+        # elif message_content[0] == "07":
+        #     info["07"] = message_content[1]  
+            
+        print("{message} is processed...")
 
     serveur.close()
 
@@ -64,17 +123,31 @@ while flag:
         raspberry.connect((raspberryIP, raspberryPort))
         flag = False
     except:
-        print("raspberry's server don't running !")
+        # print("raspberry's server don't running !")
         time.sleep(2)
 
 print("Connected to raspberry !")
 
 
 """
-GESTION DES CLIENTS
+FONCTION DE L'API
 """
 
 app = FastAPI()
+
+@app.get("/infos")
+async def infos():
+    pass
+
+
+@app.post("/")
+async def request(request: Request):
+    processRequest(request)
+    return request
+
+"""
+FONCTION DE TEST
+"""
 
 @app.get("/")
 async def root():
@@ -84,3 +157,10 @@ async def root():
 @app.get("/test")
 async def test():
     envoiMessage(raspberry, "01:00")
+
+
+@app.get("/info")
+async def info():
+    message = "09 : " + str(info["09"])
+    return {message}
+
